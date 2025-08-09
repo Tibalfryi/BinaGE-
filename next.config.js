839e4daa56baa47d1053733e6e7db64570e24d3b
Tibalfryi <pauliukovichvitali@gmail.com>
2025-08-09 17:37:29 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Включаем новый режим статического экспорта (замена старому `next export`)
-  output: 'export',
-
-  // Если используешь <Image>, без оптимизатора на статике лучше выключить оптимизацию
-  images: {
-    unoptimized: true,
-  },
-
-  // (не обязательно) Если нужны конечные слеши в путях
-  // trailingSlash: true,
+  output: 'export', // добавляем это
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
